e5ce510d7186ab4c0d4b8a9652568518a118bf30
AICI-Global-84 <aiciglobal01@gmail.com>
2024-09-23 18:25:17 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # __init__.py for registering the ImagePreviewWithWebhook node
 
-from .ImagePreviewWithWebhook import ImagePreviewWithWebhook
+from .image_preview_with_webhook import ImagePreviewWithWebhook
 
 # A dictionary that maps node class names to their corresponding classes
 NODE_CLASS_MAPPINGS = {
