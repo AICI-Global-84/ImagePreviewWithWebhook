1fd7071b2c1d2de351cce01343345a10d5e0e049
AICI-Global-84 <aiciglobal01@gmail.com>
2024-10-01 00:07:05 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,11 @@
-from .ImagePreviewWithWebhook import ImagePreviewWithWebhook
-from .ImagePreviewWithWebhook2 import ImagePreviewWithWebhook2  # Import thêm node mới
+from .ImagePreviewWithWebhook import ImagePreviewWithWebhook 
 
 NODE_CLASS_MAPPINGS = {
-    "ImagePreviewWithWebhook": ImagePreviewWithWebhook,
-    "ImagePreviewWithWebhook2": ImagePreviewWithWebhook2  # Thêm node mới vào danh sách mapping
+    "ImagePreviewWithWebhook": ImagePreviewWithWebhook
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ImagePreviewWithWebhook": "Image Preview with Webhook",
-    "ImagePreviewWithWebhook2": "Image Preview with Webhook 2"  # Tên hiển thị của node mới
+    "ImagePreviewWithWebhook": "Image Preview with Webhook"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
